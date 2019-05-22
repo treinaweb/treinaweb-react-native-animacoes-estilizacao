2693cb327e7ae70535aafbe457f9ec23187184b7
hanashiro <hanashiro@Felipes-MacBook-Pro.local>
2019-05-22 10:45:41 -0300

    aula-02-03

--- styles/mainStyles.js
+++ styles/mainStyles.js
@@ -1,15 +1,25 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 
 export const colors = {
     blue: '#0062ac',
     red: '#903232'
 }
 
+const myOSStyle = Platform.select({
+    ios: {
+        color: colors.blue
+    },
+    android: {
+        color: colors.red
+    }
+})
+
 const styles = StyleSheet.create({
     myText: {
         fontSize: 20,
-        color: colors.blue,
+        //color: colors.blue,
         //...StyleSheet.absoluteFill,
+        ...myOSStyle
     }
 });
   
